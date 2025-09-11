5e546ee2393dd8472108b8b2cc4aba7aa46a3ca5
ImMagdy <immagdy@gmail.com>
2025-09-12 02:11:29 +0300

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // --- Edit these ---
 window.CONF = {
-  API_URL: "https://script.google.com/macros/s/AKfycbxlIw2_6DrUi8JX-wKfSrYsrCa4j7oHs-_sVvrPuF8rbw-aaR46gn1Ot7ANEdqy6gvY/exec", // <— from Apps Script
+  API_URL: "https://script.google.com/macros/s/AKfycbyUD3-JLNrUTd5XspVfW6qBpMNx9y_DGoKFXqRl8tgtUGCfd1RjCN8ev1efQMk0dxnE/exec", // <— from Apps Script
   EVENT_NAME: "Scientific Day - September 12th 2025",
   // The following are informational for the UI; the server is the source of truth.
   VENUE: {
