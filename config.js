93bd8e770ea6e0bfb7992dcf78504b31162febee
ImMagdy <immagdy@gmail.com>
2025-09-12 01:56:17 +0300

    config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // --- Edit these ---
 window.CONF = {
   API_URL: "https://script.google.com/macros/s/AKfycbxlIw2_6DrUi8JX-wKfSrYsrCa4j7oHs-_sVvrPuF8rbw-aaR46gn1Ot7ANEdqy6gvY/exec", // <— from Apps Script
-  EVENT_NAME: "Morbidity and Mortality — Aug 15, 2025",
+  EVENT_NAME: "Scientific Day - September 12th 2025",
   // The following are informational for the UI; the server is the source of truth.
   VENUE: {
     LAT: 30.0768402,
