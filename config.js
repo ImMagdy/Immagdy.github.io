60e23d20fa96fe83811eac757c6022a4eac6468b
ImMagdy <immagdy@gmail.com>
2025-09-26 01:15:02 +0300

    config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // --- Edit these ---
 window.CONF = {
   API_URL: "https://script.google.com/macros/s/AKfycbwDEwHlY9y8vIjX7iGz6z4i8eTKmKR3m0fLavFePts9RMTP1B-2msbsXHSlxxN9Kdx-/exec", // <— from Apps Script
-  EVENT_NAME: "Scientific Day - September 12th 2025",
+  EVENT_NAME: "Mortality Meeting - 26th of September",
   // The following are informational for the UI; the server is the source of truth.
   VENUE: {
-    LAT: 30.0768402,
-    LNG: 31.2907355,
+    LAT: 30.0730394,
+    LNG: 31.2788604,
     RADIUS_M: 150 // match your server during testing; tighten on-site
   }
 };
