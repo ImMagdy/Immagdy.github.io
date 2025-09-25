1a09505888887892ab0fe3c7f6586ecea0243ab5
ImMagdy <immagdy@gmail.com>
2025-09-26 01:17:20 +0300

    config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // --- Edit these ---
 window.CONF = {
-  API_URL: "https://script.google.com/macros/s/AKfycbwDEwHlY9y8vIjX7iGz6z4i8eTKmKR3m0fLavFePts9RMTP1B-2msbsXHSlxxN9Kdx-/exec", // <— from Apps Script
+  API_URL: "https://script.google.com/macros/s/AKfycbyz0Rmdrej8FAamqVtqr9-mFdKIWnEFYFfyTznsrcirUXPt-_5TmhSYcwqHL29gT-Kd/exec", // <— from Apps Script
   EVENT_NAME: "Mortality Meeting - 26th of September",
   // The following are informational for the UI; the server is the source of truth.
   VENUE: {
