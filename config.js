d7105c788f7ef4cd78480bf59a739f42d98a5733
ImMagdy <immagdy@gmail.com>
2025-11-06 18:18:31 +0200

    Update event name and venue coordinates in config

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 // --- Edit these ---
 window.CONF = {
   API_URL: "https://script.google.com/macros/s/AKfycbyz0Rmdrej8FAamqVtqr9-mFdKIWnEFYFfyTznsrcirUXPt-_5TmhSYcwqHL29gT-Kd/exec", // <— from Apps Script
-  EVENT_NAME: "Mortality Meeting - 26th of September",
+  EVENT_NAME: "Scientific Day - November, 7 2025",
   // The following are informational for the UI; the server is the source of truth.
   VENUE: {
-    LAT: 30.0730394,
-    LNG: 31.2788604,
+    LAT: 30.07674,
+    LNG: 31.29096,
     RADIUS_M: 150 // match your server during testing; tighten on-site
   }
 };
